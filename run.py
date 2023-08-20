46976c54f66680092f3a72d59a0456682ab65c19
shichen-sc <>
2023-08-20 19:25:09 +0000

    write a function to generate a dictionary containing synonyms of the keywords.

--- run.py
+++ run.py
@@ -20,3 +20,39 @@ responses = {
     'track_related': 'tracking_content\n',
     'other_topic': 'other_content\n'
 }
+
+# generate dictionary containing synonyms of the key words, as different users may use different wording for same question
+def generate_synonyms_dict(key_words):
+    dict_synonyms = {}
+
+    for word in key_words:
+        synonyms = []
+        if word == 'hello':
+            for each_syn in wordnet.synsets(word):
+                for each_lem in each_syn.lemmas():
+                    synonyms.append(each_lem.name())
+            dict_synonyms[word] = set(synonyms)
+
+        elif word == 'bye':
+            for each_syn in wordnet.synsets(word):
+                for each_lem in each_syn.lemmas():
+                    synonyms.append(each_lem.name())
+            #as quit and exit can also used by user to end conversation session instead of lemmatisation of Bye
+            synonyms.extend(['quit', 'exit'])
+            dict_synonyms[word] = set(synonyms)
+        # the following words are the names of the product, thus manually extend the similar wording
+        elif word == 'satellite':
+            synonyms.extend(['satellite', 'anchor'])
+            dict_synonyms[word] = set(synonyms)
+        elif word == 'tag':
+            synonyms.extend(['tag', 'badge'])
+            dict_synonyms[word] = set(synonyms)
+        elif word == 'software':
+            synonyms.extend(['UI', 'ui', 'user interface', 'operating interface'])
+            dict_synonyms[word] = set(synonyms)
+        elif word == 'tracking':
+            synonyms.extend(['tracking', 'tracking quality', 'tracking performance', 'tracking accuracy'])
+            dict_synonyms[word] = set(synonyms)
+    
+    #print(dict_synonyms)
+    return dict_synonyms
