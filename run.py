2ce363eadcd4852bcd8f4aa88872b85d3596bce9
shichen-sc <>
2023-08-20 22:00:49 +0000

    Extend the content for dictionary responses.

--- run.py
+++ run.py
@@ -12,18 +12,29 @@ key_words = ['hello', 'bye', 'satellite', 'tag', 'software', 'tracking']
 key_list = ['greeting', 'ending', 'sat_related', 'tag_related', 'sw_related', 'track_related']
 #responses as the response content of the chat bot
 responses = {
-    'default_block': 'Hello! I am the Service Chatbot from XXX Technologies GmbH. \
-            I am happy to provide you the 2nd level service support. \
-            How can I help you?\n',
-    'greeting': "Hi, please describe your issue. For example, one tag can't be tracked anymore. \
-            If you want to quit, type in Bye or ctrl + c.\n",
+    'default_block' : 'Hello! I am the Service Robot from XXX Technologies GmbH. I am happy to provide you the 2nd level service support. How can I help you?\n', 
+    'greeting': "Hi, please describe your issue. For example, one tag can't be tracked anymore. If you want to quit, type in Bye or press ctrl + c.\n",
     'ending': 'Goodbye! I wish you further success with our products.\n',
-    'sat_related' : 'satellite_content\n',
-    'tag_related': 'tag_content\n',
-    'sw_related': 'software_content\n',
-    'track_related': 'tracking_content\n',
-    'other_topic': 'other_content\n'
-}
+    'sat_related' : 'Regarding to satellites, we have the following suggestions: \n' + 
+            '    1. Check the LED of the satellite, when everything is normal, you should see [bold]white[/] color. \n' +
+            '    2. If you see [bold blue]blue[/], it means the satellite is not being detected via network, try to restart it by unplugging and plugging the ethernet cable. \n' +
+            '    3. If you see [bold red]red[/], it means the satellite has a fatal [bold red]error[/], try to restart it by unplugging and plugging the ethernet cable. \n' +
+            '    If you still have the error behavior, you probably have a defective hardware. Please contact [bold]3rd_level_support@xxx.com[/].',
+    'tag_related': 'Regarding to tags, we have the following suggestions: \n' + 
+            '    1. Make sure they are fully charged, you should see the led blinking [bold yellow]0.5Hz yellow[/] when they are being charged correctly. \n' +
+            '    2. If they are being tracked by the system, you should see the led blinking [bold green]1Hz green[/] if you shake them. \n' +
+            '    3. If the tag has battery and not working normally, try to do a reset cycle by pressing the button on the tag for [bold]10 seconds[/] until it blinks [bold red]red[/]\n' +
+            '    If you still have the error behavior, you probably have a defective hardware. Please contact [bold]3rd_level_support@xxx.com[/].',
+    'sw_related': 'Regarding to the software issue, we have the following suggestions: \n' +
+            '    1. Make sure you have the newest release version [bold]2023.02[/], if not, please update it by pulling the image from our repository \n' +
+            '    2. If you have problem to access the ui, please check your network and make sure you are in the same subnet as our server. Sometimes a reboot of the server may help. \n' +
+            '    If you still have the error behavior, please contact [bold]3rd_level_support@xxx.com[/].',
+    'track_related': 'Regarding to the tracking issue, we have the following suggestions: \n' +
+            '    1. Check the LEDs of the satellites, they have to be [bold white]white[/]. \n' +
+            '    2. If you have overally abnormal status of satellites, make sure the satellites are in the same network subnet as the server and try to reboot the system. \n' +
+            '    3. If you have a poor accuracy in one specific area, you should confirm if you have possible interferences such as 5G communication or big metal construction. \n' +
+            '    If you still have the error behavior, please contact [bold]3rd_level_support@xxx.com[/].',
+    'other_topic': "Sorry, i don't get you. Try to describe your question more specifically. Or try to contact our 3rd level support via [bold]3rd_level_support@xxx.com[/]."
 #file to record the user's input
 filename = './session.txt'
 
